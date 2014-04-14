d02d3ed7541f1cc56c08680741e9d56ea4dbf845
Shekhar Patil <shekhar@venturit.com>
2014-04-14 17:51:37 +0530

    Added directions activity

--- PhotoSynq/src/com/photosynq/app/DirectionsActivity.java
+++ PhotoSynq/src/com/photosynq/app/DirectionsActivity.java
@@ -1,34 +1,32 @@
 package com.photosynq.app;
 
-
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.DisplayMetrics;
-import android.util.Log;
 import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.photosynq.app.db.DatabaseHelper;
 import com.photosynq.app.model.ResearchProject;
 import com.photosynq.app.utils.CommonUtils;
 
-public class ProjectDescriptionActivity extends ActionBarActivity {
+public class DirectionsActivity extends ActionBarActivity {
 
 	private String recordHash = ""; 
 	DatabaseHelper db;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_project_description);
+		setContentView(R.layout.activity_directions);
 		db = new DatabaseHelper(getApplicationContext());
-		
 		Bundle extras = getIntent().getExtras();
 		if (extras != null) {
 			recordHash = extras.getString(DatabaseHelper.C_RECORD_HASH);
@@ -41,16 +39,16 @@ public class ProjectDescriptionActivity extends ActionBarActivity {
 			//int screenHeight = displaymetrics.heightPixels;
 			
 			TextView tvProjetTitle = (TextView) findViewById(R.id.project_name);
-			TextView tvProjetDesc = (TextView) findViewById(R.id.project_desc);
+			TextView tvProjetDirections = (TextView) findViewById(R.id.project_directions);
 			TextView tvStartDate = (TextView) findViewById(R.id.start_date);
 			TextView tvEndDate = (TextView) findViewById(R.id.end_date);
 			TextView tvBeta = (TextView) findViewById(R.id.beta);
 			tvProjetTitle.setText(rp.getName());
 			tvProjetTitle.setTextSize(TypedValue.COMPLEX_UNIT_PX,(int)(screenWidth*0.06));
-			if(!"null".equals(rp.getDesc()))
+			if(!"null".equals(rp.getDir_to_collab()))
 			{
-				tvProjetDesc.setText(rp.getDesc());
-			}else{tvProjetDesc.setText(getResources().getString(R.string.no_data_found));}
+				tvProjetDirections.setText(rp.getDir_to_collab());
+			}else{tvProjetDirections.setText(getResources().getString(R.string.no_data_found));}
 			
 			if(!"null".equals(rp.getStart_date()))
 			{
@@ -66,6 +64,7 @@ public class ProjectDescriptionActivity extends ActionBarActivity {
 			{
 				tvBeta.setText(rp.getBeta());
 			}else{tvBeta.setText(getResources().getString(R.string.no_data_found));}
+
 		}
 		db.closeDB();
 	}
@@ -74,7 +73,7 @@ public class ProjectDescriptionActivity extends ActionBarActivity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.project_description, menu);
+		getMenuInflater().inflate(R.menu.directions, menu);
 		return true;
 	}
 
@@ -90,12 +89,9 @@ public class ProjectDescriptionActivity extends ActionBarActivity {
 		return super.onOptionsItemSelected(item);
 	}
 	
-	public void onParticipateClicked(View view)
+	public void onDirNextClicked(View view)
 	{
-		Intent intent = new Intent(getApplicationContext(),DirectionsActivity.class);
-		Log.d("##### Project description record hash :", recordHash);
-		intent.putExtra(DatabaseHelper.C_RECORD_HASH, recordHash);
-		startActivity(intent);
+		
 	}
-
+	
 }
