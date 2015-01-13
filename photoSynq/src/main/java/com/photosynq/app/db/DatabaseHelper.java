e5904a92220df8dac7edef4f8b2dbadb221f3407
Kalpesh Deo <kalpesh@Kalpeshs-MacBook-Pro.local>
2015-01-13 19:19:53 +0530

    Clearing cache on app installation

--- photoSynq/src/main/java/com/photosynq/app/db/DatabaseHelper.java
+++ photoSynq/src/main/java/com/photosynq/app/db/DatabaseHelper.java
@@ -27,7 +27,7 @@ import java.util.List;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
 	// Database Version
-	private static final int DATABASE_VERSION = 1;
+	private static final int DATABASE_VERSION = 2;
 	// Database Name
 	private static final String DATABASE_NAME = "PhotoSynqDB";
 	// Table Names
